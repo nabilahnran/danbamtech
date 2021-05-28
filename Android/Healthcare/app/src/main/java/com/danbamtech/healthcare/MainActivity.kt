36bc7d0767c0586b2f3053b68f319bc5cde1c9d2
jefry chiedi <jefrychiedi1999@gmail.com>
2021-05-28 22:02:58 +0700

    Initial Intent (Navigasi Awal)

--- Android/Healthcare/app/src/main/java/com/danbamtech/healthcare/MainActivity.kt
+++ Android/Healthcare/app/src/main/java/com/danbamtech/healthcare/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.danbamtech.healthcare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.danbamtech.healthcare.databinding.ActivityAmbilAntrianBinding
+import com.danbamtech.healthcare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMonitorantrian.setOnClickListener {
+            startActivity(Intent(this, DashboardAntrian::class.java))
+        }
+
+        binding.btnMenupasien.setOnClickListener {
+            startActivity(Intent(this, ScanQr::class.java))
+        }
+
+        binding.btnMenudokter.setOnClickListener {
+            startActivity(Intent(this, DashboardDokter::class.java))
+        }
     }
 }
