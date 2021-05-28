ab8399009e102da33d9e463dd012a595a877e5ff
helgaagnizar <helga18ti@mahasiswa.pcr.ac.id>
2021-05-28 20:42:40 +0700

    commit QRCode

--- Android/Healthcare/app/src/main/java/com/danbamtech/healthcare/ScanQr.kt
+++ Android/Healthcare/app/src/main/java/com/danbamtech/healthcare/ScanQr.kt
@@ -1,11 +1,200 @@
 package com.danbamtech.healthcare
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.danbamtech.healthcare.databinding.ActivityScanQrBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+
 
 class ScanQr : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
+    private lateinit var camera: Camera
+    private lateinit var cameraProvider: ProcessCameraProvider
+
+
+    companion object {
+        const val REQUEST_CAMERA_PERMISSION = 1003
+    }
+
+    private val screenAspectRatio by lazy {
+        val metrics = DisplayMetrics().also { binding.previewView.display.getRealMetrics(it) }
+        metrics.getAspectRatio()
+    }
+    private lateinit var binding: ActivityScanQrBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_qr)
+        binding = ActivityScanQrBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (isCameraPermissionGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
+        }
+
+        binding.imgFlashLight.setOnClickListener {
+            if (camera != null){
+                if (camera.cameraInfo.torchState.value == TorchState.ON){
+                    setFlashOffIcon()
+                    camera.cameraControl.enableTorch(false)
+                }else {
+                    setFlashOnIcon()
+                    camera.cameraControl.enableTorch(true)
+                }
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun bindPreview(camProvider: ProcessCameraProvider) {
+        cameraProvider = camProvider
+
+        val previewUseCase = Preview.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .setTargetAspectRatio(screenAspectRatio)
+            .build().also {
+                it.setSurfaceProvider(binding.previewView.surfaceProvider)
+            }
+        val barcodeScanner = BarcodeScanning.getClient()
+        val analysisUseCase = ImageAnalysis.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .setTargetAspectRatio(screenAspectRatio)
+            .build().also {
+                it.setAnalyzer(
+                    ContextCompat.getMainExecutor(this),
+                    { imageProxy ->
+                        processImageProxy(barcodeScanner, imageProxy)
+                    }
+                )
+            }
+        val useCaseGroup = UseCaseGroup.Builder().addUseCase(previewUseCase).addUseCase(
+            analysisUseCase
+        ).build()
+
+        camera = cameraProvider.bindToLifecycle(
+            this,
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build(),
+            useCaseGroup
+        )
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    private fun processImageProxy(barcodeScanner: BarcodeScanner, imageProxy: ImageProxy) {
+
+        // This scans the entire screen for barcodes
+        imageProxy.image?.let { image ->
+            val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+            barcodeScanner.process(inputImage)
+                .addOnSuccessListener { barcodeList ->
+                    if (!barcodeList.isNullOrEmpty()) {
+                        if (!barcodeList[0].rawValue.isNullOrEmpty()){
+                            Log.e(TAG, "processImageProxy: " + barcodeList[0].rawValue)
+                            cameraProvider.unbindAll()
+                            setFlashOffIcon()
+                            Snackbar.make(this@ScanQr,binding.clMain,
+                                "${barcodeList[0].rawValue!!}", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("Retry") {
+                                    startCamera()
+                                }
+                                .show()
+                        }
+                    }
+                }.addOnFailureListener {
+                    image.close()
+                    imageProxy.close()
+                    Log.e(TAG, "processImageProxy: ", it)
+                }.addOnCompleteListener {
+                    image.close()
+                    imageProxy.close()
+                }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setFlashOffIcon()
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        val selfPermission =
+            ContextCompat.checkSelfPermission(baseContext, Manifest.permission.CAMERA)
+        return selfPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (isCameraPermissionGranted()) {
+                startCamera()
+            } else {
+                //show custom dialog of camera permission if permission is permanently denied
+                showToast("Please allow camera permission!")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if (isCameraPermissionGranted()) {
+                    startCamera()
+                } else {
+                    showToast("Please allow camera permission!")
+                }
+            }
+        }
+    }
+
+    private fun setFlashOffIcon() {
+        binding.imgFlashLight.setImageDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.drawable.ic_flash_off_24,
+                null
+            )
+        )
+    }
+
+    private fun setFlashOnIcon(){
+        binding.imgFlashLight.setImageDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.drawable.ic_flash_on_24,
+                null
+            )
+        )
     }
 }
---
+++ Android/Healthcare/app/src/main/java/com/danbamtech/healthcare/Utils.kt
@@ -0,0 +1,27 @@
+package com.danbamtech.healthcare
+
+import android.content.Context
+import android.util.DisplayMetrics
+import android.widget.Toast
+import androidx.camera.core.AspectRatio
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+fun DisplayMetrics.getAspectRatio(): Int {
+    val ratio43Value = 4.0 / 3.0
+    val ratio169Value = 16.0 / 9.0
+
+    val previewRatio = max(this.widthPixels, this.heightPixels).toDouble() / min(
+        this.widthPixels,
+        this.heightPixels
+    )
+    if (abs(previewRatio - ratio43Value) <= abs(previewRatio - ratio169Value)) {
+        return AspectRatio.RATIO_4_3
+    }
+    return AspectRatio.RATIO_16_9
+}
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
